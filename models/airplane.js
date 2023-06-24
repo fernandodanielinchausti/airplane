5faf95fd61bffe302d3ccb3105e699a786c7f01e
Federico Cingolani <cingolanifede@gmail.com>
2023-06-24 20:47:26 -0300

    * feat(config, models): update database configuration and add airplane model and its associations.

---
+++ models/airplane.js
@@ -0,0 +1,37 @@
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class Airplane extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+
+  Airplane.init(
+    {
+      airplane_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      modelName: "airplane",
+      timestamps: false,
+    }
+  );
+
+  return Airplane;
+};
